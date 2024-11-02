5f5eab7f05e537a82bd064b216360887101f17fb
frederick-hernandez <53789782+frederick-hernandez@users.noreply.github.com>
2024-11-02 13:22:27 -0600

    finall hoy s

--- src/apis.js
+++ src/apis.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const crearProyecto  = proyecto => axios.post('https://exfinal.onrender.com/', proyecto);
-export const getItem = (id) => axios.get(`https://exfinal.onrender.com/findbyid/${id}`,id);
+export const crearProyecto  = proyecto => axios.post('https://examenfinalapi.onrender.com/proyectos/crear/', proyecto);
+export const getItem = (id) => axios.get(`https://examenfinalapi.onrender.com/proyectos/buscar/${id}`,id);
 export const updateProyecto = (id, area) => axios.put(`https://exfinal.onrender.com/${id}`, area);
-export const deleteProyecto = (id) => axios.delete(`https://exfinal.onrender.com/${id}`);
+export const deleteProyecto = (id) => axios.delete(`https://examenfinalapi.onrender.com/proyectos/delete/${id}`);
 
 export const getproyecto = () => axios.get('https://examenfinalapi.onrender.com/proyectos/todos');
